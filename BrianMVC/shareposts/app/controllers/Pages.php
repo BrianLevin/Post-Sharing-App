86285540f3a2d39911ab6494d8b4d42ed02c0618
BrianLevin123 <93602751+BrianLevin123@users.noreply.github.com>
2022-04-29 12:27:16 -0400

    created users controller and register login formn views

--- BrianMVC/shareposts/app/controllers/Pages.php
+++ BrianMVC/shareposts/app/controllers/Pages.php
@@ -1,18 +1,24 @@
 <?php
   class Pages extends Controller {
     public function __construct(){
-    
+     
     }
-
+    
     public function index(){
       $data = [
-        'title' => 'SharePosts'
+        'title' => 'SharePosts',
+        'description' => 'Simple social network built on the TraversyMVC PHP framework'
       ];
-
+     
       $this->view('pages/index', $data);
     }
 
     public function about(){
-      $this->view('pages/about');
+      $data = [
+        'title' => 'About Us',
+        'description' => 'App to share posts with other users'
+      ];
+
+      $this->view('pages/about', $data);
     }
   }
